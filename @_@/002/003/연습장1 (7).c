30d8cfbacc5698d2a11fa9a8f6a09869eefd548a
lth7000 <lth7005@naver.com>
2020-05-21 23:42:40 +0900

    test

---
+++ @_@/002/003/연습장1 (7).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int mainaaa(void)
+{
+
+	int a, b, c, d, e, i, j, k;
+	char w, x, y, z;
+	double n, m, o;
+	
+	
+
+
+		
+
+
+	printf("\n\n¥Ÿ¿Ω~!\n");
+	return 0;
+}
---
+++ @_@/002/연습장2 (2).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int mainbbb(void)
+{
+	
+
+
+
+	printf("%5d", 2);
+	
+	
+	
+	
+	return 0;
+}
